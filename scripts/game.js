1475b10af849c16794ddf571f57878bf0bc3c041
EPapas <epapas@lookbig.com>
2015-10-24 07:53:09 -0400

    Gradient Working

---
+++ scripts/game.js
@@ -0,0 +1,15 @@
+﻿var Q = Quintus()
+.include("Sprites")
+.setup({ width: 800, height: 480 });
+
+
+Q.load(["../images/gradient.jpg"], function () {
+
+    var background = new Q.Sprite({ asset: "../images/gradient.jpg", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE })
+
+    Q.gameLoop(function (dt) {
+        Q.clear();
+        background.render(Q.ctx);   
+   
+    })
+});
