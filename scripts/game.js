a9b9ddc5cd8c979be59091f14dbcf44f9ce7452f
EPapas <epapas@lookbig.com>
2015-10-24 14:49:19 -0400

    working animation

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,34 @@
 ﻿var Q = Quintus()
-.include("Sprites")
+.include("Sprites, Anim")
 .setup({ width: 800, height: 480 });
 
 Q.Sprite.extend("Player", {
     init: function (p) {
         this._super(p, {
-            asset: "../images/new.png",
+            sprite: "player",
+            sheet: "player",
             x: Q.el.width / 2,
             y: Q.el.height - 60,
             type: Q.SPRITE_FRIENDLY,
             speed: 10
-        })
+        });
+        this.add("animation");
+        this.play("default");
+
     }
 })
 Q.load(["../images/gradient.jpg", "../images/new.png", "../data/player.json"], function () {
 
 
-    Q.compileSheets("../images/new.png, ../data/player.json")
+    Q.compileSheets("../images/new.png", "../data/player.json")
+
+    Q.animations("player",{default:{ frames:[0,1,2,3], rate: 1/4}})
     var background = new Q.Sprite({ asset: "../images/gradient.jpg", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE })
     var player = new Q.Player();
     Q.gameLoop(function (dt) {
         Q.clear();
-        background.render(Q.ctx);   
+        background.render(Q.ctx);
+        player.update(dt);
         player.render(Q.ctx);
     })
 });
