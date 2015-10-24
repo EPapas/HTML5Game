22e3893dc892a6ab298505ebd3bfdd312571eaf8
EPapas <epapas@lookbig.com>
2015-10-24 08:04:52 -0400

    spaceship working

--- scripts/game.js
+++ scripts/game.js
@@ -2,14 +2,24 @@
 .include("Sprites")
 .setup({ width: 800, height: 480 });
 
-
-Q.load(["../images/gradient.jpg"], function () {
-
+Q.Sprite.extend("Player", {
+    init: function (p) {
+        this._super(p, {
+            asset: "../images/new.png",
+            x: Q.el.width / 2,
+            y: Q.el.height - 60,
+            type: Q.SPRITE_FRIENDLY,
+            speed: 10
+        })
+    }
+})
+Q.load(["../images/gradient.jpg", "../images/new.png"], function () {
+    Q.compileSheets("../images/new.png")
     var background = new Q.Sprite({ asset: "../images/gradient.jpg", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE })
-
+    var player = new Q.Player();
     Q.gameLoop(function (dt) {
         Q.clear();
         background.render(Q.ctx);   
-   
+        player.render(Q.ctx);
     })
 });
