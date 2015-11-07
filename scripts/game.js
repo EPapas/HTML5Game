393bdb4fceaab0ce06bb3f94fc85252aa4c1e5f5
EPapas <epapas@lookbig.com>
2015-11-07 09:29:45 -0500

    shot animation

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,28 @@
-﻿var clamp = function (x, min, max) {
+﻿var clamp = function(x, min, max) {
     return x < min ? min : (x > max ? max : x);
 }
 
 
 var Q = Quintus()
-.include("Sprites, Anim,Input, Touch, Scenes")
+.include("Sprites, Anim, Input, Touch, Scenes")
 .setup({ width: 1200, height: 750 })
-.controls();
+.touch();
 
+Q.input.touchControls({
+    controls: [
+        ['left', '<'],
+        ['right', '>'],
+        [],
+        [],
+        [],
+        [],
+        ['fire', 'a']
+
+]
+    
+});
+
+Q.controls();
 
 Q.Sprite.extend("Player", {
     init: function (p) {
@@ -28,22 +43,38 @@ Q.Sprite.extend("Player", {
             this.p.x -= this.p.speed;
         if (Q.inputs['right'])
             this.p.x += this.p.speed;
+      
 
         this.p.x = clamp(this.p.x, 0 + (this.p.w / 2), Q.el.width - (this.p.w / 2));
 
         }
 });
 
+Q.Sprite.extend("Shot", {
+    init: function (p) {
+        this._super(p, {
+            sprite: "shot",
+            sheet: "shot",
+            speed: 200
+        });
+        this.add("animation");
+        this.play("default"); 
+    }
+});
+
 Q.scene("mainLevel", function(stage) {
     Q.gravity = 0;
     stage.insert(new Q.Sprite({ asset: "../images/space.jpg", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE }));
     stage.insert(new Q.Player());
+    stage.insert(new Q.Shot({x:100, y:100}))
 
 });
-Q.load(["../images/space.jpg", "../images/new.png", "../data/player.json"], function () {
+Q.load(["../images/space.jpg", "../images/new.png","../images/shot.png", "../data/player.json", "../data/shot.json"], function () {
 
 
     Q.compileSheets("../images/new.png", "../data/player.json");
+    Q.compileSheets("../images/shot.png", "../data/shot.json");
     Q.animations("player", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
+    Q.animations("shot", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
     Q.stageScene("mainLevel");
 });
